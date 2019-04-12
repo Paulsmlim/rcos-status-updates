754a8583581bcc1822a56c390034539bc1eca0a5
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2019-04-12 15:18:46 -0400

    Create week_09.d

---
+++ spring_2019/week_09.d
@@ -0,0 +1,13 @@
+## Last Week's Accomplishments
+
+> This past week, I resolved all my merge conflicts and pushed the most recent code with all my features to the master branch.
+> This is for the demo presentation on 4/12. The features that I pushed to the master branch include the log in functionality,
+> registering for an account, authenticating the account, and sending the email verification to the registered account.  
+
+## This Week's Plan
+
+> Work on createPostViewController.
+
+## Anything Blocking?
+
+> No
